d885cef3d32b54b72654fbafd48eadfa1bee31ee
MonadMania <monadmania@Omega.localdomain>
2019-12-12 20:37:39 -0800

    Implemented string parsing

--- .stack-work/dist/x86_64-linux/Cabal-2.4.0.1/build/jsonparser/autogen/Paths_jsonparser.hs
+++ .stack-work/dist/x86_64-linux/Cabal-2.4.0.1/build/jsonparser/autogen/Paths_jsonparser.hs
@@ -29,12 +29,12 @@ version :: Version
 version = Version [0,1,0,0] []
 bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
 
-bindir     = "/mnt/c/Users/Operation 1/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/bin"
-libdir     = "/mnt/c/Users/Operation 1/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/lib/x86_64-linux-ghc-8.6.5/jsonparser-0.1.0.0-90MT0Ziiw4w6yesbqDz5rf-jsonparser"
-dynlibdir  = "/mnt/c/Users/Operation 1/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/lib/x86_64-linux-ghc-8.6.5"
-datadir    = "/mnt/c/Users/Operation 1/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/share/x86_64-linux-ghc-8.6.5/jsonparser-0.1.0.0"
-libexecdir = "/mnt/c/Users/Operation 1/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/libexec/x86_64-linux-ghc-8.6.5/jsonparser-0.1.0.0"
-sysconfdir = "/mnt/c/Users/Operation 1/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/etc"
+bindir     = "/mnt/d/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/bin"
+libdir     = "/mnt/d/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/lib/x86_64-linux-ghc-8.6.5/jsonparser-0.1.0.0-90MT0Ziiw4w6yesbqDz5rf-jsonparser"
+dynlibdir  = "/mnt/d/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/lib/x86_64-linux-ghc-8.6.5"
+datadir    = "/mnt/d/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/share/x86_64-linux-ghc-8.6.5/jsonparser-0.1.0.0"
+libexecdir = "/mnt/d/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/libexec/x86_64-linux-ghc-8.6.5/jsonparser-0.1.0.0"
+sysconfdir = "/mnt/d/Desktop/haskell-projects/toys/jsonparser/.stack-work/install/x86_64-linux/79655fe9df2e310fbeff3b09c4576d6353f6f7b88e521394f26ba1b1c2f8549d/8.6.5/etc"
 
 getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "jsonparser_bindir") (\_ -> return bindir)
--- exe/Main.hs
+++ exe/Main.hs
@@ -3,6 +3,7 @@ module Main where
 -- TODO: implement applicative instance for Parser.
 
 import Data.Char
+import Control.Applicative
 
 data JsonValue = JsonNull
                | JsonBool Bool
@@ -21,8 +22,25 @@ instance Functor Parser where
       (input', x) <- p input
       Just (input', f x)
 
+-- Notice that one parses the function first, then the value.
+-- This is an example of chaining parsers together.
+instance Applicative Parser where
+  pure x = Parser $ \input -> Just (input, x)
+  (Parser p1) <*> (Parser p2) = Parser $ \input -> do 
+                                  (input', f) <- p1 input
+                                  (input'', a) <- p2 input'
+                                  Just (input'', f a)
+
+instance Alternative Parser where
+  empty = Parser $ \_ -> Nothing
+  (Parser p1) <|> (Parser p2) = 
+    Parser $ \input -> p1 input <|> p2 input
+
+-- Like magic! If the parser fails to capture null, then mapping
+-- this constant function over it gives Nothing.
+-- I'm excluding all non-null cases.
 jsonNull :: Parser JsonValue
-jsonNull = undefined
+jsonNull = (\_ -> JsonNull) <$> stringP "null"
 
 -- Parse a single character, namely the first character in the input String
 charP :: Char -> Parser Char
@@ -35,10 +53,47 @@ charP x = Parser f
 -- Parse a given string, namely the initial segment of input
 -- Looking to use sequenceA to invert an applicative and a traversable
 stringP :: String -> Parser String
-stringP = undefined --sequenceA . map charP
+stringP = sequenceA . map charP
+
+-- Use the Alternative interface to attempt parsing 'true' first, then
+-- 'false' if parsing 'true' fails.
+jsonBool :: Parser JsonValue
+jsonBool = f <$> (stringP "true" <|> stringP "false")
+  where f "true"  = JsonBool True
+        f "false" = JsonBool False
+        -- This should never happen (never trust a comment like this).
+        f _       = undefined
+
+-- Pass a predicate, like isDigit, and parse the longest initial 
+-- segment for which all characters satisfy the predicate.
+spanP :: (Char -> Bool) -> Parser String
+spanP f = 
+  Parser $ \input -> 
+    let (token, rest) = span f input 
+      in Just (rest, token)
+
+jsonNumber :: Parser JsonValue
+jsonNumber = f <$> notNull (spanP isDigit)
+  where f ds = JsonNumber $ read ds -- This should always work (bad comment)
+
+-- Check if a parser of lists returns an empty list of values.
+-- Helps jsonNumber.
+notNull :: Parser [a] -> Parser [a]
+notNull (Parser p) = Parser $ \input -> do
+              (input', xs) <- p input
+              if null xs
+                 then Nothing
+                 else Just (input', xs)
+
+-- Room for improvement: add escaping support.
+stringLiteral :: Parser String
+stringLiteral = spanP (/= '"')
+
+jsonString :: Parser JsonValue
+jsonString = JsonString <$> (charP '"' *> stringLiteral  <* charP '"')
 
 jsonValue :: Parser JsonValue
-jsonValue = undefined
+jsonValue = jsonNull <|> jsonBool <|> jsonNumber <|> jsonString
 
 main :: IO ()
 main = undefined
